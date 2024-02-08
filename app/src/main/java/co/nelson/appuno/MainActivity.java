3a07ddc54cf8ab871de22a4b9316f3f48ca95414
maksilorak <maksilorak@gmail.com>
2024-02-08 09:45:15 -0500

    Clase 2
    Se agregó interactividad e interfaz gráfica

--- app/src/main/java/co/nelson/appuno/MainActivity.java
+++ app/src/main/java/co/nelson/appuno/MainActivity.java
@@ -3,17 +3,45 @@ package co.nelson.appuno;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean result = true;
-    String name = "Bob";
+   TextView nombreUsuario;
+   Button btn1,btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        nombreUsuario = (TextView) findViewById(R.id.usuario);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nombreUsuario.setText("Presioné Boton Uno");
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nombreUsuario.setText("Presioné Boton Dos");
+            }
+        });
+
 
     }
+
+    public double calcularArea(int l1,int l2){
+        return l1*l2;
+    }
+
 }
