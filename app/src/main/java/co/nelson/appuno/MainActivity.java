297c05b5b465523486eeb66da2ff0887482027dc
nelsonrechat <159147225+nelsonrechat@users.noreply.github.com>
2024-02-13 09:47:39 -0500

    Clase 3
    Ciclo de vida de una app

--- app/src/main/java/co/nelson/appuno/MainActivity.java
+++ app/src/main/java/co/nelson/appuno/MainActivity.java
@@ -3,6 +3,7 @@ package co.nelson.appuno;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
    TextView nombreUsuario;
    Button btn1,btn2;
+   String Tag = "Prueba";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +42,39 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public double calcularArea(int l1,int l2){
-        return l1*l2;
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(Tag,"Estoy en OnStart");
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(Tag,"Estoy en OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(Tag,"Estoy en OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(Tag,"Estoy en OnStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(Tag,"Estoy en OnRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(Tag,"Estoy en OnDestroy");
+    }
 }
